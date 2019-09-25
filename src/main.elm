51a72dab7d7a2ccf4439e54a292188b8317017fa
Robert K. Bell <robert.k.bell@gmail.com>
2019-09-26 09:45:46 +1000

    refactor: scratch out some basic types

--- src/main.elm
+++ src/main.elm
@@ -2,7 +2,8 @@ import Element exposing (Element, el, text, row, alignRight, fill, width, rgb255
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
-
+import Duration as Duration
+import Date
 
 main =
     Element.layout []
@@ -26,3 +27,34 @@ myElement =
         , padding 30
         ]
         (text "stylish!")
+
+type alias RangedEstimate =
+    { lowerBound: Duration.Duration
+    , upperBound: Duration.Duration
+    , confidence: Float
+    , madeOn: Date.Date}
+
+type alias PointEstimate =
+    { point: Duration.Duration
+    , confidence: Float
+    , madeOn: Date.Date }
+
+type Forecast = Ranged RangedEstimate | Point PointEstimate
+
+type alias Series =
+    { forecaster: String
+    , forecasts: List Forecast
+    }
+
+type alias Round =
+    { defaultForecast: Forecast
+    , forecasts: List Series
+    , outcome: Maybe Duration.Duration
+    }
+
+exampleRound: Round
+exampleRound =
+    { defaultForecast = Point {point= Duration.hours 3, confidence = 0.8, madeOn = Date.fromRataDie 12345 }
+    , forecasts = []
+    , outcome = Nothing
+    }
